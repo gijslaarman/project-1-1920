f03d123e94a731efd9bf28d65bb544380ce84667
gijslaarman <gijslaarman@live.nl>
2020-03-06 11:21:21 +0100

    added ugly styling..

--- js/utilities/addBookToPage.js
+++ js/utilities/addBookToPage.js
@@ -7,9 +7,9 @@ const addBookToPage = book => {
             <img src="${thisBook.coverimages[1]}" />
             <div class="details">
                 <h3>${thisBook.titles[0]}</h3>
-                ${thisBook.authors ? '<h4>Auteur(s):' + thisBook.authors.join(', ') + '</h4>' : null}
-                ${thisBook.genres ? '<h4>Genre(s):' + thisBook.genres.join(', ') + '</h4>' : null}
-                ${thisBook.languages ? '<h4>Taal:' + thisBook.languages.join(', ') + '</h4>' : null}
+                ${thisBook.authors ? '<h4>Auteur(s): ' + thisBook.authors.join(', ') + '</h4>' : null}
+                ${thisBook.genres ? '<h4>Genre(s): ' + thisBook.genres.join(', ') + '</h4>' : '<h4>Geen genre gevonden.</h4>'}
+                ${thisBook.languages ? '<h4>Taal: ' + thisBook.languages.join(', ') + '</h4>' : null}
             </div>
         </article>
     `)
